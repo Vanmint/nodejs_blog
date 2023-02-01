43d5a000d62fdb480c4d21d79eaa87177d84a460
Vanmint <hothihongvan151@gmaill.com>
2023-02-01 13:42:28 +0700

    update 1/2/2022

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ const { engine } = require ('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+const route = require('./routes')
+
 // Use static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 app.use(morgan('combined'));
+
+// Midderware để xử lý dữ liệu form data
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// xử lý dữ liệu từ code js lên (sử dụng XMLHttpRequest, fetch, axios, ...)
+app.use(express.json());
 
 // Template engine
 app.engine('hbs', engine({
@@ -22,13 +33,28 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 // console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Route init
+route(app);
+
+
+// app.get('/', (req, res) => {
+//   res.render('home');
+// });
+
+// app.get('/news', (req, res) => {
+//   res.render('news');
+// });
+
+// app.get('/search', (req, res) => {
+//   // console.log(req.query.q)
+//   res.render('search');
+// });
+
+// app.post('/search', (req, res) => {
+//   console.log(req.body)
+//   res.send('');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
